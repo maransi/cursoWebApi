e091b888d8c578c9b76672e6828792628abfbdec
maransi <maransi>
2022-08-05 13:57:27 -0300

    Teste de alteração do projeto

--- src/Api.Domain/Entities/UserEntity.cs
+++ src/Api.Domain/Entities/UserEntity.cs
@@ -10,6 +10,6 @@ namespace Api.Domain.Entities
     public class UserEntity: BaseEntity
     {
         public string Nome { get; set; }
-        public string Email { get; set; }
+        public string Email { get; set; } 
     }
 }
