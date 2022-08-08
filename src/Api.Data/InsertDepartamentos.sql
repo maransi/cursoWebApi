ae93aaf42dc1e7503507564afb40993a7da3d822
maransi <maransi>
2022-08-08 13:25:18 -0300

    Novos Scripts

---
+++ src/Api.Data/InsertDepartamentos.sql
@@ -0,0 +1,50 @@
+/*
+-- Query: SELECT cod_departamento AS Id, nom_departamento AS nome
+FROM tab_departamentos
+-- Date: 2022-08-08 13:23
+*/
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (1,'FINANCEIRO');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (2,'COBRANCA');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (3,'CREDITO');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (4,'COMERCIAL');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (5,'CONTABILIDADE');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (6,'TI');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (7,'COMPLIANCE');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (8,'RECURSOS HUMANOS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (9,'LIMPEZA');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (10,'ARQUIVO');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (11,'MENOR APRENDIZ');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (12,'JURÍDICO');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (13,'RECEPÇÃO');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (14,'DIRETORIA');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (15,'TELEFONIA');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (16,'FORNECEDOR');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (18,'LOCAÇÃO');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (19,'IMPOSTOS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (20,'FUNCIONÁRIOS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (21,'COMISSÃO S/COBRANÇA');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (22,'CORRETORES');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (23,'DESPESAS BANCÁRIAS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (24,'SÓCIOS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (25,'SPC / SERASA');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (26,'VEÍCULOS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (27,'ASSESSORIA');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (28,'IMOBILIZADO');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (29,'PROCESSOS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (30,'ADVOGADOS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (31,'TRANSFERÊNCIAS ENTRE BANCOS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (32,'RENDIMENTOS S/APLICAÇÕES');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (33,'BLOQUEIO JUDICIAL');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (34,'DESBLOQUEIO JUDICIAL');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (35,'MULTAS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (36,'SISBACEN');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (37,'APLICAÇÃO FINANCEIRA');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (38,'RESGATE FINANCEIRO');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (39,'CUSTAS CARTORIAIS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (40,'TITULOS DE CREDITOS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (41,'OUVIDORIA');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (42,'VIAGENS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (43,'CONTINGENCIA');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (44,'COBRADORES');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (45,'PRODUTOS');
+INSERT INTO `departamentos` (`Id`,`nome`) VALUES (46,'ATENDIMENTO DIGITAL');
